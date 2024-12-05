53167b37868d98a6a29ef25438cfd448bce92998
Jerry Bruwes <jbruwes@gmail.com>
2024-12-05 16:15:13 +0200

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 import { version } from "vue";
 
 const dest = "assets";
-const src = "../node_modules/vue/dist/vue.esm-browser.prod.js";
+const src = "./node_modules/vue/dist/vue.esm-browser.prod.js";
 const rename: RenameFunc = (fileName, fileExtension) =>
   `${fileName}-${version}.${fileExtension}`;
 const targets = [{ dest, rename, src }];
@@ -23,19 +23,12 @@ const app = fileURLToPath(new URL("..", import.meta.url));
 const alias: AliasOptions = { "@": ".", app };
 const resolve: UserConfig["resolve"] = { alias };
 const manifest = true;
-const outDir = "../public/monolit";
 const manualChunks = (id: string) =>
   id.split("node_modules/")[1]?.split("/")[0]?.replace(/^@/, "");
 const output = { manualChunks };
 const external = ["vue"];
 const rollupOptions: BuildOptions["rollupOptions"] = { external, output };
-// const target = "esnext";
-const build: BuildOptions = {
-  manifest,
-  outDir,
-  rollupOptions,
-  //  target,
-};
+const build: BuildOptions = { manifest, rollupOptions };
 const define: object = {
   __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   __VUE_PROD_DEVTOOLS__: true,
