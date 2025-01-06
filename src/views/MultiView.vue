1a4eba7622b6a340406f5add75ee494ba35593eb
Jerry Bruwes <jbruwes@gmail.com>
2025-01-06 15:36:02 +0200

    modified:   src/views/MultiView.vue

--- src/views/MultiView.vue
+++ src/views/MultiView.vue
@@ -1,9 +1,9 @@
 <template>
   <div
     :class="the.class"
-    :id="the.id"
-    :key="the.id"
-    :role="the.id === that?.id ? 'main' : undefined"
+    :id="the.id ?? v4()"
+    :key="the.id ?? v4()"
+    :role="the.id === that?.id ? 'main' : 'section'"
     ref="refs"
     un-cloak
     v-for="the in $siblings"
@@ -20,11 +20,18 @@
   </div>
 </template>
 <script setup lang="ts">
+/* -------------------------------------------------------------------------- */
+/*                                   Imports                                  */
+/* -------------------------------------------------------------------------- */
+
 import type { TPage } from "@vues3/shared";
-import type { Ref } from "vue";
+import type { UseScrollOptions } from "@vueuse/core";
+import type { ComputedRef, Ref } from "vue";
+import type { Router } from "vue-router";
 
 import { deep, pages } from "@vues3/shared";
 import { useIntersectionObserver, useScroll } from "@vueuse/core";
+import { v4 } from "uuid";
 import { computed, onUnmounted, ref, watch } from "vue";
 import { useRouter } from "vue-router";
 
@@ -40,57 +47,108 @@ import {
   threshold,
 } from "../stores/monolit";
 
-const refs: Ref<HTMLElement[]> = ref([]);
-const router = useRouter();
-const templates = computed(() => {
+/* -------------------------------------------------------------------------- */
+/*                                  Constants                                 */
+/* -------------------------------------------------------------------------- */
+
+const stops: (() => void)[] = [];
+
+/* -------------------------------------------------------------------------- */
+/*                                Computations                                */
+/* -------------------------------------------------------------------------- */
+
+const templates: ComputedRef<object> = computed(() => {
   {
-    const [[key, value]] = promises;
+    const [[key, value] = []] = promises;
     promises.clear();
-    promises.set(key, value);
+    if (key && value) promises.set(key, value);
   }
   return Object.fromEntries(
     $siblings.value.map((value) => [value.id, getAsyncComponent(value)]),
   ) as object;
 });
-const template = ({ id }: TPage) =>
-  templates.value[id as keyof object] as object;
-const intersecting = computed(
-  () => new Map($siblings.value.map(({ id }) => [id, false])),
+
+/* -------------------------------------------------------------------------- */
+
+const intersecting: ComputedRef<Map<string, boolean>> = computed(
+  () => new Map($siblings.value.map(({ id = v4() }) => [id, false])),
 );
-const $intersecting = ref(new Map(intersecting.value));
-const onStop = () => {
+
+/* -------------------------------------------------------------------------- */
+/*                                 References                                 */
+/* -------------------------------------------------------------------------- */
+
+const refs: Ref<HTMLElement[]> = ref([]);
+
+/* -------------------------------------------------------------------------- */
+
+const $intersecting: Ref<Map<string, boolean>> = ref(
+  new Map(intersecting.value),
+);
+
+/* -------------------------------------------------------------------------- */
+/*                                 Composables                                */
+/* -------------------------------------------------------------------------- */
+
+const router: Router = useRouter();
+
+/* -------------------------------------------------------------------------- */
+/*                                  Functions                                 */
+/* -------------------------------------------------------------------------- */
+
+const template: ({ id }: TPage) => object = ({ id }) =>
+  templates.value[id as keyof object];
+
+/* -------------------------------------------------------------------------- */
+
+const onStop: UseScrollOptions["onStop"] = () => {
   if (!paused.value && that.value && $siblings.value.length) {
     const { scrollX, scrollY } = window;
     const [first] = $siblings.value;
     const [root] = pages.value;
-    const { $children: [{ id }] = [{}] } = root;
-    const name =
-      !Math.floor(scrollX) && !Math.floor(scrollY) && first.id === id
-        ? root.id
-        : ([...intersecting.value.entries()].find(([, value]) => value)?.[0] ??
-          [...$intersecting.value.entries()].find(([, value]) => value)?.[0] ??
-          first.id);
-    scroll.value = false;
-    router.push({ name }).catch(() => {});
+    if (root && first) {
+      const { $children: [{ id } = {}] = [] } = root;
+      const name =
+        !Math.floor(scrollX) && !Math.floor(scrollY) && first.id === id
+          ? root.id
+          : ([...intersecting.value.entries()].find(
+              ([, value]) => value,
+            )?.[0] ??
+            [...$intersecting.value.entries()].find(
+              ([, value]) => value,
+            )?.[0] ??
+            first.id);
+      scroll.value = false;
+      router.push({ name }).catch(() => {});
+    }
   }
 };
-useScroll(window, { behavior, onStop });
-const callback = ([
-  {
-    isIntersecting,
-    target: { id },
-  },
-]: IntersectionObserverEntry[]) => {
+
+/* -------------------------------------------------------------------------- */
+
+const callback: IntersectionObserverCallback = ([
+  { isIntersecting, target: { id } = {} } = {},
+] = []) => {
   $intersecting.value = new Map(intersecting.value);
-  intersecting.value.set(id, isIntersecting);
+  if (id && isIntersecting !== undefined)
+    intersecting.value.set(id, isIntersecting);
 };
-const stops: (() => void)[] = [];
-const clearStops = () => {
+
+/* -------------------------------------------------------------------------- */
+
+const clearStops: () => void = () => {
   stops.forEach((stop: () => void) => {
     stop();
   });
   stops.length = 0;
 };
+
+/* -------------------------------------------------------------------------- */
+/*                                    Main                                    */
+/* -------------------------------------------------------------------------- */
+
+useScroll(window, { behavior, onStop });
+
 watch(
   refs,
   (value) => {
@@ -106,5 +164,8 @@ watch(
   },
   { deep },
 );
+
 onUnmounted(clearStops);
+
+/* -------------------------------------------------------------------------- */
 </script>
