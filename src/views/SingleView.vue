52fcc1ca6af6b85b20fe87bdc75541739df6e525
Jerry Bruwes <jbruwes@icloud.com>
2025-03-14 18:00:01 +0200

    modified:   src/views/SingleView.vue

--- src/views/SingleView.vue
+++ src/views/SingleView.vue
@@ -13,22 +13,14 @@ import { v4 } from "uuid";
 import { computed, onUpdated } from "vue";
 
 import { module, promises, resolve, that } from "../stores/monolit";
-
-/* -------------------------------------------------------------------------- */
-
 const { id } = defineProps<{ id?: string }>();
-
 const the = computed(() => (id ? atlas[id as keyof object] : that.value));
-
 const is = computed(() => {
   const [[key, value] = []] = promises;
   promises.clear();
   if (key && value) promises.set(key, value);
   return the.value && module(the.value);
 });
-
-/* -------------------------------------------------------------------------- */
-
 onUpdated(() => {
   if (id && the.value) resolve(the.value);
 });
