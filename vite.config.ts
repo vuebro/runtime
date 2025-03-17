b13979991c51bc8be155a8ecc617654c9de51a80
Jerry Bruwes <jbruwes@icloud.com>
2025-03-17 17:43:54 +0200

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,37 @@
-import type { RenameFunc } from "vite-plugin-static-copy";
-
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
-import { version } from "vue";
-const app = fileURLToPath(new URL("..", import.meta.url)),
-  alias = { "@": ".", app },
-  base = "./",
-  define = {
+
+import { dependencies } from "./package.json";
+export default defineConfig({
+  base: "./",
+  build: {
+    manifest: true,
+    rollupOptions: {
+      external: ["vue", "vue-router"],
+      output: {
+        manualChunks: (id) =>
+          id.split("node_modules/")[1]?.split("/")[0]?.replace(/^@/, ""),
+      },
+    },
+  },
+  define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
     __VUE_PROD_DEVTOOLS__: true,
   },
-  dest = "assets",
-  external = ["vue"],
-  manifest = true,
-  resolve = { alias },
-  src = "./node_modules/vue/dist/vue.esm-browser.prod.js";
-const manualChunks = (id: string) =>
-    id.split("node_modules/")[1]?.split("/")[0]?.replace(/^@/, ""),
-  rename: RenameFunc = (fileName, fileExtension) =>
-    `${fileName}-${version}.${fileExtension}`;
-const output = { manualChunks },
-  rollupOptions = { external, output },
-  build = { manifest, rollupOptions },
-  targets = [{ dest, rename, src }],
-  plugins = [vue(), viteStaticCopy({ targets })];
-export default defineConfig({ base, build, define, plugins, resolve });
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: ["vue", "vue-router"].map((value) => ({
+        dest: "assets",
+        rename: (fileName: string, fileExtension: string) =>
+          `${fileName}-${dependencies[value as keyof typeof dependencies].replace(/^\^/, "")}.${fileExtension}`,
+        src: `./node_modules/${value}/dist/${value}.esm-browser.prod.js`,
+      })),
+    }),
+  ],
+  resolve: {
+    alias: { "@": ".", app: fileURLToPath(new URL("..", import.meta.url)) },
+  },
+});
