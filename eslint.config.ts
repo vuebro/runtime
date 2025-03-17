888012b04bebea00ada1bf9b09064d71ea5e1247
Jerry Bruwes <jbruwes@icloud.com>
2025-03-17 17:42:26 +0200

    new file:   eslint.config.ts

---
+++ eslint.config.ts
@@ -0,0 +1,43 @@
+import eslint from "@eslint/js";
+import eslintPluginImportX from "eslint-plugin-import-x";
+import perfectionist from "eslint-plugin-perfectionist";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+import tseslint, { configs, parser } from "typescript-eslint";
+import vueParser from "vue-eslint-parser";
+export default tseslint.config(
+  { ignores: ["**/dist"] },
+  {
+    rules: {
+      "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
+      "import-x/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/eslint.config.ts", "**/vite.config.ts"],
+          optionalDependencies: false,
+        },
+      ],
+    },
+  },
+  {
+    languageOptions: {
+      globals: { ...globals.browser },
+      parser: vueParser,
+      parserOptions: {
+        extraFileExtensions: [".vue"],
+        parser,
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  eslint.configs.recommended,
+  eslintPluginImportX.flatConfigs.recommended,
+  eslintPluginImportX.flatConfigs.typescript,
+  configs.strictTypeChecked,
+  configs.stylisticTypeChecked,
+  ...pluginVue.configs["flat/recommended"],
+  perfectionist.configs["recommended-natural"],
+  eslintPluginPrettierRecommended,
+);
