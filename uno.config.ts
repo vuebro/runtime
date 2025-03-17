127e0ebb29f098c351494b65e1d6ce533305fa51
Jerry Bruwes <jbruwes@icloud.com>
2025-03-17 17:43:15 +0200

    modified:   uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -1,16 +1,14 @@
-import type { Preset } from "@unocss/core";
-
 import attributify from "@unocss/preset-attributify";
 import icons from "@unocss/preset-icons/browser";
 import tagify from "@unocss/preset-tagify";
 import typography from "@unocss/preset-typography";
 import wind3 from "@unocss/preset-wind3";
-const cdn = "https://cdn.jsdelivr.net/npm/",
-  presets: Preset[] = [
+export default {
+  presets: [
     wind3(),
     typography(),
-    icons({ cdn }),
+    icons({ cdn: "https://cdn.jsdelivr.net/npm/" }),
     tagify(),
     attributify(),
-  ];
-export default { presets };
+  ],
+};
