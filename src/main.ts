7b4c8edaef63c1883f54e9707448a777131585d3
Jerry Bruwes <jbruwes@icloud.com>
2025-03-17 18:05:01 +0200

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import type { Preset } from "@unocss/core";
-import type { RuntimeOptions } from "@unocss/runtime";
 import type { TImportmap, TPage } from "@vues3/shared";
 import type { Component } from "vue";
-import type { RouteRecordRaw } from "vue-router";
 
 import { createHead } from "@unhead/vue";
 import webFonts from "@unocss/preset-web-fonts";
@@ -23,11 +21,6 @@ import defaults from "../uno.config";
 import vueApp from "./App.vue";
 import { router, setScroll } from "./stores/monolit";
 import "./style.css";
-const getChildren = (
-  component: RouteRecordRaw["component"],
-  name: RouteRecordRaw["name"],
-  path: RouteRecordRaw["path"],
-) => [{ component, name, path }] as RouteRecordRaw[];
 const id = computed(() => router.currentRoute.value.name),
   initRouter = (async () => {
     const [{ imports }, [page = {} as TPage]] = await Promise.all(
@@ -44,43 +37,35 @@ const id = computed(() => router.currentRoute.value.name),
     await nextTick();
     window.app.provide("pages", shallowReadonly(atlas));
     pages.value.forEach(({ flat, id: name, loc, parent, path: relative }) => {
-      const component = () => import("./views/SingleView.vue");
       if (relative !== undefined) {
         const alias = loc
-            ?.replaceAll(" ", "_")
-            .replace(/^\/?/, "/")
-            .replace(/\/?$/, "/"),
-          children = getChildren(
-            (parent?.flat ?? flat)
-              ? () => import("./views/MultiView.vue")
-              : component,
-            name,
-            "",
-          ),
-          path = relative.replace(/^\/?/, "/").replace(/\/?$/, "/");
+          ?.replaceAll(" ", "_")
+          .replace(/^\/?/, "/")
+          .replace(/\/?$/, "/");
         router.addRoute({
           ...(alias && loc ? { alias } : { undefined }),
-          children,
-          component,
-          path,
+          children: [
+            {
+              component:
+                (parent?.flat ?? flat)
+                  ? () => import("./views/MultiView.vue")
+                  : () => import("./views/SingleView.vue"),
+              name,
+              path: "",
+            },
+          ],
+          component: () => import("./views/SingleView.vue"),
+          path: relative.replace(/^\/?/, "/").replace(/\/?$/, "/"),
         });
       }
     });
-    const component = () => import("./views/NotFoundView.vue"),
-      name = "404",
-      path = "/:pathMatch(.*)*";
-    router.addRoute({ component, name, path });
+    router.addRoute({
+      component: () => import("./views/NotFoundView.vue"),
+      name: "404",
+      path: "/:pathMatch(.*)*",
+    });
   })();
 const rootElement = () => document.getElementById("app") ?? undefined;
-const ready: RuntimeOptions["ready"] = async (runtime) => {
-  const { toggleObserver } = runtime;
-  setScroll(runtime);
-  await initRouter;
-  window.app.use(router);
-  window.app.mount(rootElement());
-  toggleObserver(true);
-  return false;
-};
 window.app = createApp(vueApp as Component);
 window.app.use(createHead());
 window.app.provide("id", readonly(id));
@@ -90,7 +75,19 @@ window.app.provide("id", readonly(id));
       (await (response.ok ? response : new Response("[]")).json()) as string[],
     );
   defaults.presets.push(webFonts({ customFetch, fonts }) as Preset);
-  await initUnocssRuntime({ defaults, ready, rootElement });
+  await initUnocssRuntime({
+    defaults,
+    ready: async (runtime) => {
+      const { toggleObserver } = runtime;
+      setScroll(runtime);
+      await initRouter;
+      window.app.use(router);
+      window.app.mount(rootElement());
+      toggleObserver(true);
+      return false;
+    },
+    rootElement,
+  });
 })().catch(consoleError);
 window.console.info(
   "⛵",
