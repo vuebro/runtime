72469e24dbf591dfaf06a1379689ab21c716e5df
Jerry Bruwes <jbruwes@icloud.com>
2025-05-04 16:10:39 +0200

    modified:   uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -2,10 +2,10 @@ import attributify from "@unocss/preset-attributify";
 import icons from "@unocss/preset-icons/browser";
 import tagify from "@unocss/preset-tagify";
 import typography from "@unocss/preset-typography";
-import presetWind4 from "@unocss/preset-wind4";
+import wind4 from "@unocss/preset-wind4";
 export default {
   presets: [
-    presetWind4(),
+    wind4(),
     typography(),
     icons({ cdn: "https://cdn.jsdelivr.net/npm/" }),
     tagify(),
