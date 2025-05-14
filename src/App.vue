9bc5fb4f5144467f008a19efabcc4ed6bf9840e6
Jerry Bruwes <jbruwes@icloud.com>
2025-05-14 16:31:10 +0200

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
   </router-view>
 </template>
 <script setup lang="ts">
+import type { TPage } from "@vues3/shared";
 import type { MetaFlat } from "unhead/types";
 
 import { getIcon, iconLoaded, loadIcon } from "@iconify/vue";
@@ -11,14 +12,17 @@ import { useHead, useSeoMeta } from "@unhead/vue";
 import { pages } from "@vues3/shared";
 import { computed, ref, watch } from "vue";
 import { useRoute } from "vue-router";
+
 const route = useRoute();
 const a = computed(() => pages.value.find(({ id }) => id === route.name)),
   favicon = ref(""),
+  jsonld = ref(""),
   ogUrl = computed(
     () =>
       a.value?.to &&
       `${window.location.origin}${a.value.to === "/" ? "" : a.value.to}`,
   );
+
 watch(a, async (value) => {
   let href = "/favicon.ico";
   if (value?.icon) {
@@ -37,22 +41,46 @@ useHead({
     [favicon, "icon", "icon"],
     [ogUrl, "canonical"],
   ].map(([href, rel, key]) => ({ href, key, rel })),
+  script: [
+    {
+      id: "application/ld+json",
+      innerHTML: jsonld,
+      type: "application/ld+json",
+    },
+  ],
 });
 useSeoMeta({
-  description: () => a.value?.description ?? undefined,
-  keywords: () => a.value?.keywords.join(),
-  ogDescription: () => a.value?.description ?? undefined,
-  ogImage: () =>
-    a.value?.images
-      .filter(({ url }) => url)
-      .map(({ alt = "", url }) => ({
-        alt,
-        url: url ? `${window.location.origin}/${url}` : "",
-      })) ?? [],
-  ogTitle: () => a.value?.title,
-  ogType: () =>
-    a.value?.type ? (a.value.type as MetaFlat["ogType"]) : undefined,
+  description: computed(() => a.value?.description),
+  keywords: computed(() => a.value?.keywords.join()),
+  ogDescription: computed(() => a.value?.description),
+  ogImage: computed(
+    () =>
+      a.value?.images
+        .filter(({ url }) => url)
+        .map(({ alt = "", url }) => ({
+          alt,
+          url: `${window.location.origin}/${url}`,
+        })) ?? [],
+  ),
+  ogTitle: computed(() => a.value?.title),
+  ogType: computed(
+    () => a.value?.type as MetaFlat["ogType"] | null | undefined,
+  ),
   ogUrl,
-  title: () => a.value?.title ?? "",
+  title: computed(() => a.value?.title),
 });
+watch(
+  () =>
+    (
+      a.value as
+        | (TPage & {
+            jsonld: Promise<object>;
+          })
+        | undefined
+    )?.jsonld,
+  async (value) => {
+    jsonld.value =
+      JSON.stringify(await value) || '{"@context":"https://schema.org"}';
+  },
+);
 </script>
