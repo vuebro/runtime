bf2e1c818cd15bc781394631bf3136c6d5f0c23e
Jerry Bruwes <jbruwes@icloud.com>
2025-06-18 14:06:34 +0200

    new file:   src/views/RootView.vue

---
+++ src/views/RootView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="the?.enabled" :id="the?.id ?? v4()" :class="the?.class" un-cloak>
+    <component
+      :is
+      :id="the?.id"
+      un-cloak
+      @vue:mounted="resolve(the)"
+    ></component>
+  </div>
+</template>
+<script setup lang="ts">
+import { pages } from "@vuebro/shared";
+import { v4 } from "uuid";
+import { computed } from "vue";
+
+import { module, resolve } from "../stores/monolit";
+
+const [the] = pages.value,
+  is = computed(() => the && module(the));
+</script>
