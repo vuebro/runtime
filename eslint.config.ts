5b9dc291544b33b422401f27d878415501908559
Jerry Bruwes <jbruwes@icloud.com>
2025-10-16 12:30:21 +0200

    refactor: update ESLint config with gitignore and type fixes

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,7 @@
+import type { ConfigWithExtendsArray } from "@eslint/config-helpers";
+
 import eslint from "@eslint/js";
+import gitignore from "eslint-config-flat-gitignore";
 import { flatConfigs } from "eslint-plugin-import-x";
 import perfectionist from "eslint-plugin-perfectionist";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
@@ -9,38 +12,24 @@ import { configs, parser } from "typescript-eslint";
 import vueParser from "vue-eslint-parser";
 
 export default defineConfig(
-  { ignores: ["**/dist"] },
-  {
-    rules: {
-      "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
-      "import-x/no-extraneous-dependencies": [
-        "error",
-        {
-          devDependencies: ["**/eslint.config.ts", "**/vite.config.ts"],
-          optionalDependencies: false,
-        },
-      ],
-    },
-  },
+  gitignore(),
   {
     languageOptions: {
-      globals: { ...globals.browser },
+      globals: globals.browser,
       parser: vueParser,
       parserOptions: {
         extraFileExtensions: [".vue"],
         parser,
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
   eslint.configs.recommended,
-  //@ts-expect-error Argument of type 'PluginFlatConfig' is not assignable to parameter of type 'InfiniteArray<ConfigWithExtends>'.
-  flatConfigs.recommended,
-  flatConfigs.typescript,
+  flatConfigs.recommended as ConfigWithExtendsArray,
+  flatConfigs.typescript as ConfigWithExtendsArray,
   configs.strictTypeChecked,
   configs.stylisticTypeChecked,
-  ...pluginVue.configs["flat/recommended"],
+  pluginVue.configs["flat/recommended"],
   perfectionist.configs["recommended-natural"],
   eslintPluginPrettierRecommended,
 );
