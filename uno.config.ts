8ab55d44ba85961490b2edb85db72c9c40c3e1dd
Jerry Bruwes <jbruwes@icloud.com>
2025-10-20 17:47:40 +0200

    Simplify UnoCSS configuration with shared config

--- uno.config.ts
+++ uno.config.ts
@@ -1,14 +1,3 @@
-import attributify from "@unocss/preset-attributify";
-import icons from "@unocss/preset-icons/browser";
-import tagify from "@unocss/preset-tagify";
-import typography from "@unocss/preset-typography";
-import wind4 from "@unocss/preset-wind4";
-export default {
-  presets: [
-    wind4({ preflights: { reset: true } }),
-    typography(),
-    icons({ cdn: "https://cdn.jsdelivr.net/npm/" }),
-    tagify(),
-    attributify(),
-  ],
-};
+import config from "@vuebro/configs/uno";
+
+export default config;
