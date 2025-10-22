d850c57332cf4f21381df44c21b1780070592094
Jerry Bruwes <jbruwes@icloud.com>
2025-10-22 16:17:15 +0200

    Update UnoCSS config with presets and defineConfig

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,5 @@
 import config from "@vuebro/configs/uno";
+import presets from "@vuebro/configs/uno/presets";
+import { defineConfig } from "unocss";
 
-export default config;
+export default defineConfig({ presets: presets(), ...config });
