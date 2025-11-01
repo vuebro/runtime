80f7355429e358a19719e2babba8be391f35a9b9
Jerry Bruwes <jbruwes@icloud.com>
2025-11-01 17:07:51 +0200

    Update root component props and store imports

--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
 Suspense
-  div(v-if="the?.enabled", :id="the?.id", :class="the?.class", un-cloak)
-    component(:is, :pid="the?.id", @vue:mounted="resolve(the)")
+  div(v-if="the?.enabled", :id="the.id", :class="the.class", un-cloak)
+    component(:is, :id="the.id", @vue:mounted="root.resolve(undefined)")
 </template>
+
 <script setup lang="ts">
 import { pages } from "@vuebro/shared";
 import { computed } from "vue";
 
-import { module, resolve } from "../stores/monolit";
+import { module, root } from "@/stores/monolit";
 
 const [the] = pages.value,
-  is = computed(() => the && module(the));
+  is = computed(() => the && module(the.id));
 </script>
