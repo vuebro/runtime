461590c303544fd6ddfbe88867f8ed9281606c09
Jerry Bruwes <jbruwes@icloud.com>
2025-11-01 17:06:53 +0200

    Refactor main.ts with improved scroll behavior and routing logic

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,60 @@
-import type { TPage } from "@vuebro/shared";
-
 import { createHead } from "@unhead/vue/client";
 import initUnocssRuntime from "@unocss/runtime";
 import presets from "@vuebro/configs/uno/presets";
 import { atlas, fetching, nodes, pages } from "@vuebro/shared";
-import { toReactive } from "@vueuse/core";
+import { toReactive, useScroll } from "@vueuse/core";
 import { consola } from "consola/browser";
-import { createApp, watch } from "vue";
+import { ofetch as customFetch } from "ofetch";
+import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 import vueApp from "@/App.vue";
-import { pause, promises, scroll } from "@/stores/monolit";
+import {
+  $these,
+  intersecting,
+  promises,
+  root,
+  routeName,
+  that,
+} from "@/stores/monolit";
+import "@/style.css";
 import notFoundView from "@/views/NotFoundView.vue";
 import pageView from "@/views/PageView.vue";
 import rootView from "@/views/RootView.vue";
-import "@/style.css";
 
-const app = createApp(vueApp);
+const [[page = {}], fonts] = (
+    await Promise.all(
+      ["index", "fonts"].map((file) => fetching(`${file}.json`)),
+    )
+  ).map((value) => value ?? []),
+  app = createApp(vueApp),
+  { pathname } = new URL(document.baseURI);
 
 consola.info(
   "👊 VueBro / https://github.com/vuebro / runtime ver.:",
   __APP_VERSION__,
 );
 
-const jsonld = {
-    async get(this: TPage) {
-      if (this.id) await fetching(`./pages/${this.id}.jsonld`);
-    },
-  },
-  { pathname } = new URL(document.baseURI);
+nodes.push(page);
 
-(async () => {
-  const [[page = {}], fonts] = (
-    await Promise.all(
-      ["index.json", "fonts.json"].map((file) => fetching(file)),
-    )
-  ).map((value) => value ?? []);
-  nodes.push(page);
-  await initUnocssRuntime({
-    defaults: {
-      presets: presets({
-        webFontsOptions: {
-          fonts: Object.fromEntries(
-            fonts.map((font: string) => [
-              font.toLowerCase().replace(/ /g, "_"),
-              font,
-            ]),
-          ),
-        },
-      }),
-    },
-    ready: ({ extractAll, toggleObserver }) => {
-      watch(pause, (value) => {
-        toggleObserver(value);
-      });
-      const router = createRouter({
+await initUnocssRuntime({
+  defaults: {
+    presets: presets({
+      webFontsOptions: {
+        customFetch,
+        fonts: Object.fromEntries(
+          fonts.map((font: string) => [
+            font.toLowerCase().replace(/ /g, "_"),
+            font,
+          ]),
+        ),
+      },
+    }),
+  },
+  ready: ({ extractAll, toggleObserver }) => {
+    let scrollLock = false;
+
+    const router = createRouter({
         history: createWebHistory(pathname),
         routes: [
           ...pages.value
@@ -78,41 +78,65 @@ const jsonld = {
             }),
           { component: notFoundView, name: "404", path: "/:pathMatch(.*)*" },
         ],
-        scrollBehavior: async ({ hash, name, path }) => {
+        scrollBehavior: async ({ hash, name }) => {
           if (name) {
-            const [{ promise } = {}] = promises.values(),
-              { index, parent: { flat } = {} } =
-                (path === "/"
-                  ? atlas.value[name as keyof object]?.$children[0]
-                  : atlas.value[name as keyof object]) ?? {};
-            pause.value = true;
-            await promise;
-            await Promise.all(
-              [...promises.values()].map(({ promise }) => promise),
-            );
-            await extractAll();
-            const routerScrollBehavior = scroll.value && {
-              behavior: "smooth" as ScrollOptions["behavior"],
-              ...(hash || (flat && index)
-                ? { el: hash || `#${String(name)}` }
-                : { left: 0, top: 0 }),
-            };
-            pause.value = false;
-            scroll.value = true;
-            return routerScrollBehavior;
-          } else return false;
+            routeName.value = name;
+            if (scrollLock) scrollLock = false;
+            else {
+              const { index, parent: { flat } = {} } = that.value ?? {};
+              toggleObserver(true);
+              await root.promise;
+              await Promise.all(
+                [...promises.values()].map(({ promise }) => promise),
+              );
+              await extractAll();
+              toggleObserver(false);
+              if ("requestIdleCallback" in window)
+                await new Promise((resolve) => requestIdleCallback(resolve));
+              else {
+                await new Promise((resolve) => requestAnimationFrame(resolve));
+                await new Promise((resolve) => setTimeout(resolve));
+              }
+              return {
+                behavior: "smooth" as ScrollOptions["behavior"],
+                ...(hash || (flat && index)
+                  ? { el: hash || `#${String(name)}` }
+                  : { left: 0, top: 0 }),
+              };
+            }
+          }
+          return false;
+        },
+      }),
+      { x, y } = useScroll(window, {
+        onStop: () => {
+          const [first] = $these.value,
+            [root] = pages.value;
+          if (root && first) {
+            const { $children: [{ id } = {}] = [] } = root;
+            const name =
+              !Math.floor(x.value) && !Math.floor(y.value) && first.id === id
+                ? root.id
+                : ([...intersecting.entries()].find(
+                    ([, value]) => value,
+                  )?.[0] ?? first.id);
+            if (name !== routeName.value) {
+              scrollLock = true;
+              router.push({ name }).catch(consola.error);
+            }
+          }
         },
       });
-      router.beforeEach(({ path }) =>
-        path !== decodeURI(path) ? decodeURI(path) : undefined,
-      );
-      app.use(router);
-      return false;
-    },
-    rootElement: () => document.getElementById("app") ?? undefined,
-  });
-  pages.value.forEach((page) => {
-    Object.defineProperties(page, { jsonld });
-  });
-  app.use(createHead()).provide("pages", toReactive(atlas)).mount("#app");
-})().catch(consola.error);
+
+    router.beforeEach(({ path }) =>
+      path !== decodeURI(path) ? decodeURI(path) : undefined,
+    );
+
+    app.use(router);
+
+    return false;
+  },
+  rootElement: () => document.getElementById("app") ?? undefined,
+});
+
+app.use(createHead()).provide("pages", toReactive(atlas)).mount("#app");
