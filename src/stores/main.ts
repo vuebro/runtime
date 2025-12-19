1a5539b6a56de9280311a110efc106f9e68a1cd0
Jerry Bruwes <jbruwes@icloud.com>
2025-12-19 18:41:19 +0200

    Update dependencies and remove unused packages

--- src/stores/main.ts
+++ src/stores/main.ts
@@ -7,18 +7,31 @@ import { frontmatterPlugin } from "@mdit-vue/plugin-frontmatter";
 import { sfcPlugin } from "@mdit-vue/plugin-sfc";
 import { tocPlugin } from "@mdit-vue/plugin-toc";
 import { abbr } from "@mdit/plugin-abbr";
+import { alert } from "@mdit/plugin-alert";
+import { align } from "@mdit/plugin-align";
+import { attrs } from "@mdit/plugin-attrs";
+import { demo } from "@mdit/plugin-demo";
 import { dl } from "@mdit/plugin-dl";
+import { embed } from "@mdit/plugin-embed";
+import { figure } from "@mdit/plugin-figure";
 import { footnote } from "@mdit/plugin-footnote";
 import { icon } from "@mdit/plugin-icon";
+import { imgLazyload } from "@mdit/plugin-img-lazyload";
+import { imgMark } from "@mdit/plugin-img-mark";
+import { imgSize } from "@mdit/plugin-img-size";
 import { ins } from "@mdit/plugin-ins";
 import { katex } from "@mdit/plugin-katex";
 import { mark } from "@mdit/plugin-mark";
+import { ruby } from "@mdit/plugin-ruby";
+import { spoiler } from "@mdit/plugin-spoiler";
+import { stylize } from "@mdit/plugin-stylize";
 import { sub } from "@mdit/plugin-sub";
 import { sup } from "@mdit/plugin-sup";
+import { tab } from "@mdit/plugin-tab";
 import { tasklist } from "@mdit/plugin-tasklist";
+import { ElementTransform } from "@nolebase/markdown-it-element-transform";
 import loadModule from "@vuebro/loader-sfc";
 import { fetching, sharedStore } from "@vuebro/shared";
-// import { linksPlugin } from "@vuepress/markdown";
 import MarkdownIt from "markdown-it";
 import pluginMdc from "markdown-it-mdc";
 import { computed, defineAsyncComponent, reactive, toRefs } from "vue";
@@ -29,20 +42,68 @@ interface PromiseWithResolvers<T> {
   resolve: (value: PromiseLike<T> | T) => void;
 }
 
+let transformNextLinkCloseToken = false;
+
 const { kvNodes, nodes } = $(toRefs(sharedStore));
-const md = MarkdownIt({ html: true, linkify: true, typographer: true })
-  .use(katex)
+const md = MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+})
+  .use(ElementTransform, {
+    transform(token) {
+      switch (token.type) {
+        case "link_close":
+          if (transformNextLinkCloseToken) {
+            token.tag = "RouterLink";
+            transformNextLinkCloseToken = false;
+          }
+          break;
+        case "link_open": {
+          const href = token.attrGet("href") ?? "/";
+          if (!URL.canParse(href)) {
+            token.tag = "RouterLink";
+            token.attrSet("to", href);
+            token.attrs?.splice(token.attrIndex("href"), 1);
+            transformNextLinkCloseToken = true;
+          }
+          break;
+        }
+      }
+    },
+  })
   .use(abbr)
+  .use(alert)
+  .use(align)
+  .use(attrs)
+  .use(demo)
   .use(dl)
-  .use(icon)
+  .use(figure)
   .use(footnote)
+  .use(icon)
+  .use(imgLazyload)
+  .use(imgMark)
+  .use(imgSize)
   .use(ins)
+  .use(katex)
   .use(mark)
+  .use(ruby)
+  .use(spoiler)
+  .use(stylize)
   .use(sub)
   .use(sup)
+  .use(tab)
   .use(tasklist)
+  .use(embed, {
+    config: [
+      {
+        name: "youtube",
+        setup: (id: string) =>
+          `<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allowfullscreen></iframe>`,
+      },
+    ],
+  })
   .use(pluginMdc)
-  // .use(linksPlugin)
   .use(frontmatterPlugin)
   .use(tocPlugin)
   .use(componentPlugin)
@@ -82,13 +143,6 @@ export const promiseWithResolvers = <T>() => {
     defineAsyncComponent(async () => {
       const env: MarkdownItEnv = {};
       md.render((await fetching(`./docs/${id}.md`)) ?? "", env);
-
-      console.log(`${env.sfcBlocks?.template?.content ?? ""}
-${env.sfcBlocks?.script?.content ?? ""}
-${env.sfcBlocks?.scriptSetup?.content ?? ""}
-${env.sfcBlocks?.styles.map(({ content }) => content).join("\n") ?? ""}
-`);
-
       return loadModule(
         `${env.sfcBlocks?.template?.content ?? ""}
 ${env.sfcBlocks?.script?.content ?? ""}
